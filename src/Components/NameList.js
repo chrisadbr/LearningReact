0fbea730d61c22ea90d60a59228e207c189abd37
Christian <christian@pop-os.localdomain>
2020-10-21 12:59:24 +0300

    react tutorial

--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Bruce', 'Wayne', 'David']
-    const nameList = names.map(name => <h2>{ name }</h2>)
+    const profile = [
+        {
+            name: 'Bruce Wayne',
+            age: 40,
+            location: 'Ruvuma',
+            jobTitle: 'Software developer'
+        },
+        {
+            name: 'Roger Miller',
+            age: 60,
+            location: 'Younde',
+            jobTitle: 'Retired footballer'
+        },
+        {
+            name: 'Paul George',
+            age: 29,
+            location: 'Los angeles',
+            jobTitle: 'Pro basketball player'
+        },
+        {
+            name: 'TD Jakes',
+            age: 65,
+            location: 'Atlanta',
+            jobTitle: 'Reverend'
+        }
+    ]
+    const person = profile.map(people => (
+        <p>My name is {people.name}, I live in {people.location}, Iam a {people.jobTitle}</p>
+    ))
     return (
-        <div>{ nameList }</div>
+        <div>{ person }</div>
     )
 }
 
